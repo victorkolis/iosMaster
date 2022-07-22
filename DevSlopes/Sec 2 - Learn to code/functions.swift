26dcb49f740bd6efd21a7ab138e437bb7cff5935
Victor deMatos <58838266+victorkolis@users.noreply.github.com>
2022-07-22 01:23:13 -0300

    devslopes directory refactor

---
+++ DevSlopes/Sec 2 - Learn to code/functions.swift
@@ -0,0 +1,20 @@
+// dimensions
+var lenght = 5
+var width = 10
+
+var area = lenght * width
+
+
+// shape 1
+var lenght_1 = 6
+var width_1 = 12
+
+var area_1 = lenght_1 * width_1
+print("The are of shape 1 is: \(area_1)")
+
+// shape 2
+var lenght_2 = 7
+var width_2 = 15
+
+var area_2 = lenght_2 * width_2
+print("The are of shape 1 is: \(area_2)")
